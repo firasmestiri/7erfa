f5ea43930067c0c5da0e047f8d6c7bfe52d9abd7
khaled <mohamed.khaled.dridi177@gmail.com>
2023-11-20 23:15:01 +0100

    searchbar

--- src/Components/HomePage/HomePageSearchBar.js
+++ src/Components/HomePage/HomePageSearchBar.js
@@ -1,5 +1,39 @@
 import React from "react";
-
+import { useState } from "react";
 export default function HomePageSearchBar() {
-  return <div>hihi</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Implement your search logic here, using the 'searchTerm' state
+    console.log(`Searching for: ${searchTerm}`);
+  };
+  return (
+    <div className="input-group">
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <input
+          type="search"
+          id="form1"
+          className="form-control"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <input
+          type="search"
+          id="form2"
+          className="form-control"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+      </div>
+      <button type="button" className="btn btn-primary" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+  );
 }
