f8a471e8391c3891c6c1bdc0f9bfaaa22d463c28
BIm0 Z <mouhibbouzamita@gmail.com>
2023-12-09 07:32:14 +0100

    fetch workers from the server

--- src/Components/Services/ServicesPage.js
+++ src/Components/Services/ServicesPage.js
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FilterServices from "./FilterServices/FilterServices";
-import ServicesData from "../../public/ServicesData";
-import ServicesItems from "./ServicesItems";
+import WorkerCard from "./WorkerCard";
+import axios from "axios"
+import { apiURL } from "../../apiConfig";
 
 export default function ServicesPage() {
+  const [workers, setWorkers] = useState([]);
+
+  useEffect(() => {
+    const url = apiURL + "/workers";
+    console.log(url);
+    axios.get(url).then(response => {
+      setWorkers(response.data.workers);
+      console.log(response.data.workers);
+    }).catch(error => {
+      console.log(error);
+    })
+  }, []);
+
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       <FilterServices />
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-        {ServicesData.map((service) => (
-          <ServicesItems key={service.id} service={service} />
-        ))}
+        {workers.map((worker, index) => {
+          return <WorkerCard key={index} worker={worker} />;
+        })}
       </div>
     </div>
   );
