c23c622f8f91203073693c255b994f31f8e91ef1
khaled <mohamed.khaled.dridi177@gmail.com>
2023-12-20 16:04:52 +0100

    wa

--- src/store.js
+++
@@ -1,7 +0,0 @@
-// store.js
-import { createStore } from "redux";
-import rootReducer from "./reducers"; // Adjust the path based on your project structure
-
-const store = createStore(rootReducer);
-
-export default store;
